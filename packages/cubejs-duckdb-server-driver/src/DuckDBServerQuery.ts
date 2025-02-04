1cfd36b52de048e6a893bc413d0b57749c1f68ec
Alexander Kolov <me@alexkolov.com>
2025-02-04 23:55:09 +0900

    Wrong driver

--- packages/cubejs-duckdb-server-driver/src/DuckDBServerQuery.ts
+++ packages/cubejs-duckdb-server-driver/src/DuckDBServerQuery.ts
@@ -73,6 +73,7 @@ export class DuckDBServerQuery extends BaseQuery {
     templates.functions.DATETRUNC = 'DATE_TRUNC({{ args_concat }})';
     templates.functions.LEAST = 'LEAST({{ args_concat }})';
     templates.functions.GREATEST = 'GREATEST({{ args_concat }})';
+    templates.filters.time_range_filter = '{{ column }}::timestamptz >= {{ from_timestamp }} AND {{ column }}::timestamptz <= {{ to_timestamp }}';
     return templates;
   }
 }
