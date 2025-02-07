4dbac28f291f382644a5fb3731764e2a8136d1a3
Alexander Kolov <me@alexkolov.com>
2025-02-07 22:11:04 +0900

    Handle decimal type

--- packages/cubejs-duckdb-server-driver/src/transform.ts
+++ packages/cubejs-duckdb-server-driver/src/transform.ts
@@ -3,7 +3,11 @@ import { DataType, Field } from 'apache-arrow';
 export function transformValue(field: Field, value: any) {
   if (DataType.isDate(field.type) || DataType.isTimestamp(field.type)) {
     return new Date(value).toISOString();
-  } else if (typeof value === 'number' || typeof value === 'bigint') {
+  }
+  else if (DataType.isDecimal(field.type)) {
+    return value.toString();
+  }
+  else if (typeof value === 'number' || typeof value === 'bigint') {
     return value.toString();
   }
   return value;
