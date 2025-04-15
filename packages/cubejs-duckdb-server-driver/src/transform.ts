a82f4859ad3cb5ce5873bc812a3af734fb1591db
Alexander Kolov <me@alexkolov.com>
2025-04-15 14:10:02 +0900

    Modify transform function

--- packages/cubejs-duckdb-server-driver/src/transform.ts
+++ packages/cubejs-duckdb-server-driver/src/transform.ts
@@ -1,14 +1,23 @@
 import { DataType, Field } from 'apache-arrow';
 
 export function transformValue(field: Field, value: any) {
-  if (DataType.isDate(field.type) || DataType.isTimestamp(field.type)) {
+  console.log('DuckDB Server transform value', { field, value });
+
+  if (value === null || value === undefined || DataType.isNull(field.type)) {
+    return value;
+  } if (typeof value === 'bigint') {
+    return value.toString();
+  } else if (DataType.isInt(field.type)) {
+    return Number(value);
+  } else if (DataType.isFloat(field.type)) {
+    return Number(value);
+  } else if (DataType.isBool(field.type)) {
+    return Boolean(value);
+  } else if (DataType.isDecimal(field.type)) {
+    return value.toString();
+  } else if (DataType.isDate(field.type) || DataType.isTimestamp(field.type)) {
     return new Date(value).toISOString();
   }
-  else if (DataType.isDecimal(field.type)) {
-    return value.toString();
-  }
-  else if (typeof value === 'number' || typeof value === 'bigint') {
-    return value.toString();
-  }
+
   return value;
 }
